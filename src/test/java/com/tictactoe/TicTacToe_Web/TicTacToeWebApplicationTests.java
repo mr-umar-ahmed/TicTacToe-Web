159953e2276ae546390f7bea2810f8aab833d043
umar ahmed <umarahmedahmed24@gmail.com>
2025-02-02 17:16:58 +0530

    Initial project setup with Spring Boot

---
+++ src/test/java/com/tictactoe/TicTacToe_Web/TicTacToeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tictactoe.TicTacToe_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
